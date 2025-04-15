c44a337fdfc6abc6d9dacded93bf47d4e72f9f0b
Eric Kabis de Saint Chamas <eric.kabis_de_saint_chamas@etu.minesparis.psl.eu>
2025-04-15 15:23:44 -0400

    structure pour les qa

--- src/app/quiz/normal/qa/QuizQAClient.tsx
+++ src/app/quiz/normal/qa/QuizQAClient.tsx
@@ -1,33 +1,48 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type Question = {
   question: string;
   answer: string;
 };
 
-export default function QuizQAClient({ questions }: { questions: Question[] }) {
-  const [index, setIndex] = useState(0);
+export default function QuizThematiqueClient({ questions }: { questions: Question[] }) {
+  const router = useRouter();
+  const [index, setIndex] = useState(Math.floor(Math.random() * questions.length));
   const [showAnswer, setShowAnswer] = useState(false);
 
   const next = () => {
-    setIndex(i => (i + 1 < questions.length ? i + 1 : 0));
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    setIndex(randomIndex);
     setShowAnswer(false);
   };
 
   return (
     <main style={{ padding: "2rem" }}>
-      <h2>{questions[index].question}</h2>
+      <div className="header">
+        <h1>Quiz thématique</h1>
+      </div>
+      <div className="back-quiz">
+        <div className="quiz-container">
+          <h2 className="quiz-question">{questions[index].question}</h2>
 
-      {showAnswer ? (
-        <p><strong>{questions[index].answer}</strong></p>
-      ) : (
-        <button onClick={() => setShowAnswer(true)}>Afficher la réponse</button>
-      )}
-
-      <button onClick={next}>Question suivante</button>
+          {showAnswer ? (
+            <p><strong>{questions[index].answer}</strong></p>
+          ) : (
+            
+            <button className="quiz-buttons answer" onClick={() => setShowAnswer(true)}>Afficher la réponse</button>
+            
+          )}
+          
+          <button className="quiz-buttons next" onClick={next}>Question suivante</button>
+          
+        </div>
+      </div>
+      <div>
+        <button className="retour" onClick={() => router.push("/quiz/normal")}> Retour </button>
+      </div>
     </main>
   );
 }
