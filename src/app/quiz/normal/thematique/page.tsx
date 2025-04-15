14729eb31c95b699dc887cad584197ff7f3e4401
Eric Kabis de Saint Chamas <eric.kabis_de_saint_chamas@etu.minesparis.psl.eu>
2025-04-15 13:04:34 -0400

    la séparation server/client fonctionne !

--- src/app/quiz/normal/thematique/page.tsx
+++ src/app/quiz/normal/thematique/page.tsx
@@ -1,25 +1,7 @@
-// src/app/quiz/page.tsx
-"use client"
 import { loadQuestions } from "@/lib/loadQuestions";
-import {useRouter} from "next/navigation";
+import QuizThematiqueClient from "./QuizThematiqueClient";
 
-export default function QuizTheme() {
-  const router = useRouter();
-  const questions = loadQuestions();
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  const { question, answer } = questions[randomIndex];
-
-  return (
-    <main>
-      <h1>Quiz de culture générale</h1>
-      <p><strong>Mot-clé :</strong> {question}</p>
-      <details>
-        <summary>Voir la réponse</summary>
-        <p>{answer}</p>
-      </details>
-      <div>
-        <button className="retour" onClick={() => router.push("/quiz/normal")}> Retour </button>
-      </div>
-    </main>
-  );
+export default function QuizThematiquePage() {
+  const questions = loadQuestions(); // lecture via fs
+  return <QuizThematiqueClient questions={questions} />;
 }
