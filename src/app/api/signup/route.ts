0e82d5d651b4338d638c6e60acd63547baee4d0a
Eric Kabis de Saint Chamas <eric.kabis_de_saint_chamas@etu.minesparis.psl.eu>
2025-04-16 17:33:31 -0400

    créer un compte fonctionne !!!

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,17 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { Client } from 'pg';
-import bcrypt from 'bcrypt';
+import pool from '../../../lib/db';  // Importe le pool de connexions
 
 export async function POST(req: NextRequest) {
   const { username, email, password } = await req.json();
 
-  // Connexion à la base de données
-  const client = new Client();
-  await client.connect();
-
   try {
     // Vérifie si l'utilisateur existe déjà
-    const userCheck = await client.query(
+    const userCheck = await pool.query(
       'SELECT * FROM users WHERE email = $1',
       [email]
     );
@@ -21,10 +16,11 @@ export async function POST(req: NextRequest) {
     }
 
     // Chiffre le mot de passe
+    const bcrypt = require('bcrypt');
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insérer l'utilisateur dans la base de données avec le mot de passe chiffré
-    const result = await client.query(
+    // Insère l'utilisateur dans la base de données avec le mot de passe chiffré
+    const result = await pool.query(
       'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id',
       [username, email, hashedPassword]
     );
@@ -33,8 +29,5 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Erreur interne du serveur' }, { status: 500 });
-  } finally {
-    await client.end();
   }
 }
-
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,8 @@
 import { Pool } from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config(); // Charge les variables du fichier .env
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
